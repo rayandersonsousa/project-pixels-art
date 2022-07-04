217dc7ff82f5ed541b29eb2fc33537d925359269
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-07-04 18:42:01 -0300

    Requsito 8 e algumas correções

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', unselectColor);
   colors[index].addEventListener('click', selectColor);
 }
+
+function paintPixel(event) {
+  if (event.target.classList.contains('pixel')) {
+    const selectedColor = document.querySelector('.selected');
+    const newColor = selectedColor.style.backgroundColor;
+    event.target.style.backgroundColor = newColor;
+  }
+}
+
+document.addEventListener('click', paintPixel);
